c8f546a2f061236e08d62cfaf722507de3031ec4
RLant <rebeccalant@gmail.com>
2021-03-25 14:00:15 -0400

    typo in script name

--- scripts.js
+++ scripts.js
@@ -1,10 +1,4 @@
 $(function () {
-
-  // Fade in lightbox after half a second
-  // $(".lightbox").delay(500).fadeIn(1000);
-
-S
+ $("p").css("background-color", "rgba(180, 180, 30, 0.8");
 });
 
-//here is one example on how you would use this in the real world
-//You can have a lightbox fadein after a delay and ask for someone's email. 
