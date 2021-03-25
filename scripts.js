ba95ba231b4e2da2021c3307d8ed6b2990a9b045
RLant <rebeccalant@gmail.com>
2021-03-25 14:08:05 -0400

    notes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 $(function () {
  $("p").css("background-color", "rgba(180, 180, 30, 0.8");
+ $(".red-box").css("background-color", "rgba(250, 100, 300, 0.8");
+ $("#list").css("background-color", "rgba(250, 150, 300, 0.8");
+
+ $('input[type="text"]').css("background-color", "rgba(250, 150, 300, 0.8");
+
 });
 
+//using the css fucntion, we can edit any element with CSS style. 
+
+// you can also select elements by input type like you can see on line 6. Just be careful that when you are using quotes inside of quotes that they are different than the ones you have it in. In this case, we have single quotes and then double quotes around the text type. 
